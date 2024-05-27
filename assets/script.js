7dded2bba5d796d778dfbf96cf254fe581f405f5
Syed Burhan <155990533+syedzada198@users.noreply.github.com>
2024-05-27 17:01:39 +0500

    adjust button

--- assets/script.js
+++ assets/script.js
@@ -84,14 +84,6 @@ function validateForm() {
 
 
 $(document).ready(function () {
-  $(window).scroll(function () {
-    if ($(this).scrollTop() > 100) {
-      $('.go-to-top').fadeIn();
-    } else {
-      $('.go-to-top').fadeOut();
-    }
-  });
-
   $(".go-to-top").click(function (e) {
     e.preventDefault();
     $("html, body").animate({ scrollTop: 0 }, "slow");
